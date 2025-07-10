94981c54368382b08ca18b94f4413973cf8aab28
Your Name <you@example.com>
2025-07-10 07:27:00 -0600

    termine backend

---
+++ backend/loginapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def login_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return JsonResponse({'status':'ok'}, status=200)
+        else: 
+            return JsonResponse({'status':'fail'}, status=404)
+    else:
+        return JsonResponse({'status':'unknown method'}, status=404)
+        
